89e5d49622f113a3e6fb2dedd862c03fe42293cb
Romaska-uwu <149205053+Romaska-uwu@users.noreply.github.com>
2025-09-26 11:46:49 +0300

    Add files via upload

---
+++ Лр ВРПО №2/ЛР ВРПО №2/Лр ВРПО №2/Лр ВРПО №2.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+//AdamovichKarianna2006
+// 41 64 61 6D 6F 76 69 63 68 4B 61 72 69 61 6E 6E 61 32 30 30 36
+// 41 64 61 6D 6F 76 69 63 68 4B 61 72 69 61 6E 6E 61 32 30 30 36
+// 41 64 61 6D 6F 76 69 63 68 4B 61 72 69 61 6E 6E 61 32 30 30 36
+
+//АдамовичКарианнаПавловна2006
+// C0 E7 E0 EC EE E2 E8 E7 CA E0 F0 E8 E0 ED ED E0 61 CF E0 E2 EB EE E2 ED E0 32 30 30 36
+// d090,d0b4,d0b0,d0bc,d0be,d0b2,d0b8,d187,d09a,d0b0,d180,d0b8,d0b0,d0bd,d0bd,d0b0, 32, 30, 30, 36
+// d090,d0b4,d0b0,d0bc,d0be,d0b2,d0b8,d187,d09a,d0b0,d180,d0b8,d0b0,d0bd,d0bd,d0b0, 32, 30, 30, 36
+
+//Адамович2006Karianna
+// C0 E7 E0 EC EE E2 E8 E7 32 30 30 36 4B 61 72 69 61 6E 6E 61
+// d090,d0b4,d0b0,d0bc,d0be,d0b2,d0b8,d187,32,30,30,36,4b,61,72,69,61,6e,6e,61
+// d090,d0b4,d0b0,d0bc,d0be,d0b2,d0b8,d187,32,30,30,36,4b,61,72,69,61,6e,6e,61
+
+int main()
+{
+    int number = 0x12345678;
+    char hello[] = "Hello,";
+    char lfie[] = "AdamovichKarianna2006";
+    char rfie[] = "АдамовичКарианнаПавловна2006";
+    char lr[] = "Адамович2006Karianna";
+
+    wchar_t Lfie[] = L"AdamovichKarianna2006";
+    wchar_t Rfie[] = L"АдамовичКарианнаПавловна2006";
+    wchar_t LR[] = L"Адамович2006Karianna";
+
+    std::cout << hello << lfie << std::endl;
+    return 0;
+}
---
+++ Лр ВРПО №3/Лр ВРПО №3/Лр ВРПО №3/Лр ВРПО №3.cpp
@@ -0,0 +1,61 @@
+﻿#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	char symbol_A, symbol_B; 
+	int choice, code_difference, Symbol_A1;
+
+	cout << "Выберете действие, которое хотите проверить :" << endl << endl;
+	cout << "1.Определение разницы значений кодов в Windows-1251 буквы латинского алфавита в прописном и строчном написании; " << endl << endl;
+	cout << "2.Определение разницы значений кодов в Windows-1251 буквы русского алфавита в прописном и строчном написании; " << endl << endl;
+	cout << "3.Вывод в консоль кода символа, соответствующего введённой цифре;" << endl << endl;
+	cout << "4.Выход из программы." << endl << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1: {
+		cout << "Введите с клавиатуры заглавную букву латинского алфавита" << endl;
+		cin >> symbol_A;
+		cout << "Введите с клавиатуры строчную букву латинского алфавита" << endl;
+		cin >> symbol_B;
+
+		code_difference = abs(symbol_B - symbol_A);
+		cout << "разницы значений кодов букв латинского алфавита в прописном и строчном написании  = " << code_difference << endl << endl;
+		break;
+	}
+
+	case 2: {
+		cout << "Введите с клавиатуры заглавную букву русского алфавита" << endl;
+		cin >> symbol_A;
+		cout << "Введите с клавиатуры строчную букву русского алфавита" << endl;
+		cin >> symbol_B;
+
+		code_difference = abs(symbol_A - symbol_B);
+		cout << "разницы значений кодов букв русского алфавита в прописном и строчном написании  = " << code_difference << endl << endl;
+		break;
+	}
+
+	case 3: {
+		cout << "Введите с клавиатуры цифру" << endl;
+		cin >> symbol_A;
+		Symbol_A1 = (char)symbol_A;
+
+		cout << "Вы ввели цифру с кодом " << Symbol_A1 << endl;
+		break;
+	}
+
+	case 4: {
+
+		break;
+	}
+
+	default: {
+		cout << "Не правильно написан символ, повторите попытку" << endl;
+		break;
+	}
+
+	}
+
+
+}
